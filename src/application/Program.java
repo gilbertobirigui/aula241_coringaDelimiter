03bf2393fdb6e9db24c4b5408859147a0553c9f3
gilbertobirigui <gfilmesbirigui@hotmail.com>
2023-11-28 09:30:12 -0300

    Aula241 coring delimiter Contravariancia

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,20 +17,47 @@ public class Program {
     
     public static void main(String[] args){
         
-        List<Integer> intList = new ArrayList<Integer>();
-        intList.add(10);
-        intList.add(5);
+        // agora criar lista object que pode ser qualquer tipo
+        List<Object> myObjs = new ArrayList<>();
+        myObjs.add("Maria");  // String sub tipo de object
+        myObjs.add("Alex");
       
-        List<? extends Number> list = intList;
+        
+        
+        /* vou declarar uma lista myNums
+        do tipo ? qualquer tipo super Number
+        ou seja 
+        pode ser Number ou qualquer tipo super Number no caso Object
+        
+                                   object
+                                     |
+        ----------------------------------------------------------
+        |                            |                           |
+        NUMBER                      BOOLEAN                  CHARACTER
+          |
+          |
+        --------------------------------------------------------
+           |           |         |        |         |          |
+        Integer      Byte       Long     Shot      Float      Double
+        
+        
+        
+        */
+        
+        List<? super Number> myNums = myObjs;
+        
+        myNums.add(10);
+        myNums.add(3.14);   // posso add qualquer tipo number tipo - int double float etc
     
-        Number x = list.get(0);  // ele deixa eu acessar elementos
-    
+        // tambem posso add qualquer tipo Object - String
+        // so que nao posso acessar 
+        
+        // erro de compilaçao
+        Number x = myNums.get(0);      
        
-        // erro de compilacao porque ele nao sabe 20 qual tipo é
-        // isso a gente chama de covariancia
-        // operacao get é permitida 
-        // operacao inserir nao é permitida
-        //list.add(20); 
+        // eu nao passo acessar minha lista e guardar na minha variavel do tipo Number
+        // porque o tipo dessa lista pode ser um tipo ser tipo super tipo de Number
+     
     }
     
     
